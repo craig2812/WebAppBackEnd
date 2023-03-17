b7b1c7fc396266e76207f64504aa9698d8d8e3ac
craig.farrell <Craig.Farrell@blackbaud.me>
2023-03-17 23:29:40 +0000

    tidy up 2

--- server.ts
+++ server.ts
@@ -1,13 +1,6 @@
 const express = require( "express");
 const {connectToDatabase}  =require("./services/database.service");
 const contentRouter =require("./routes/content.router");
-const cors = require("cors");
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-import {json} from 'body-parser'
-const mongoose =require('mongoose')
-const { connectDB } =require("./config/db")
-
 const app = express();
 const port = 8080; // default port to listen
 
@@ -29,33 +22,4 @@ connectToDatabase()
     });
 
 
-    require('dotenv').config({path:'./config.env'});
-
-
-    
-    
-    
-    // const app= express();
-    // const PORT= process.env.PORT || 8000;
-    // const errorHandler = require('./middleware/error')
-    
-    // //connect to db
-    // connectDB()
-    
-    // app.use(express.json());
-    // app.use("/api/auth", require("./routes/auth"));
-    // app.use("/api/private", require("./routes/private"));
-    
-    // //ErrorHandler (Should be last piece of middleware)
-    // app.use(errorHandler);
-    
-    // const server=app.listen(
-    //     PORT,()=>{
-    //         console.log(`Server is running on port ${PORT}`)
-    //     }
-    // )
-    // process.on("unhandledRejection",(error,promise)=>{
-    //     console.log(`Logged Error: ${error}`);
-    //     server.close(()=>process.exit(1))
-    
-    // })
+    require('dotenv').config({path:'./.env'});
